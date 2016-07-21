79d484c863d6ab824a8b789eb01cec5b8b347002
morrillo <odoo@debian>
2016-07-21 18:13:16 +0000

    Added amount line function

--- sale_old.py
+++ sale_old.py
@@ -40,6 +40,20 @@ class sale_order_line(osv.osv):
 			temp_value = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
 			return temp_value * (1 - (line.second_discount or 0.0) / 100.0)
 
+	def _amount_line(self, cr, uid, ids, field_name, arg, context=None):
+		tax_obj = self.pool.get('account.tax')
+		cur_obj = self.pool.get('res.currency')
+		res = {}
+		if context is None:
+			context = {}
+		for line in self.browse(cr, uid, ids, context=context):
+			price = line.price_unit * (1 - (line.discount or 0.0) / 100.0) * (1 - (line.second_discount or 0.0) / 100)
+	            	taxes = tax_obj.compute_all(cr, uid, line.tax_id, price, line.product_uom_qty, line.product_id, line.order_id.partner_id)
+	                cur = line.order_id.pricelist_id.currency_id
+		        res[line.id] = cur_obj.round(cr, uid, cur, taxes['total'])
+        	return res
+
+
 sale_order_line()
 
 class stock_move(osv.osv):
